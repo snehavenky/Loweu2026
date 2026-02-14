5f59fb613f89c19b5ef9f86ec67d1d21e1cf3a9e
Sneha <snehavenky@users.noreply.github.com>
2026-02-14 15:53:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ with col1:
 
 with col2:
     # Black Cat (Noto Emoji)
-    st.image("https://fonts.gstatic.com/s/e/notoemoji/latest/1f408_200d_1f311/512.png", width=100)
+    st.image("https://openmoji.org/data/color/svg/1F408-200D-1F311.svg", width=100)
     st.subheader("Meemee")
     st.write("🐾 **Loweus: Cozy**")
     st.caption("Rating: 12/10. Claims your legs as her permanent snuggle territory. No movement allowed.")
